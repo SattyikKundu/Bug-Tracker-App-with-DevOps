9f670a420bcd105df610440f4c58eda59158f702
Sattyik Kundu <sattyikkundu@gmail.com>
2025-08-13 22:49:52 -0400

    Created isseLoader.js file as middleware helper that 'loads' selected project issue based on using issue Id and project Id. Part of project issues routes.
    Will complete testing of issues Routes files via Swagger API later on.

---
+++ server/middleware/issueLoader.js
@@ -0,0 +1,37 @@
+// server/middleware/issueLoader.js
+import mongoose from "mongoose";                 // For ObjectId check
+import Issue from "../models/issueModel.js";     // Issue model
+import Project from "../models/projectModel.js"; // Project model
+
+const isValidId = (id) => {  // Validate id helper function
+    mongoose.Types.ObjectId.isValid(id); 
+}
+
+export const loadIssue = async (req,res,next)=>{ // Load issue by ':id' and its project
+  try{
+    const {id} = req.params; // Issue id param
+
+    if(!isValidId(id)) { // If inavlid Id, return 400 (error) response
+        return res.status(400).json({error:"Invalid issue id."}); 
+    } 
+
+    const issue = await Issue.findById(id).lean(); // Retrieve issue as a noraml object
+
+    if(!issue) { // If no issue, return 404 (error) reponse
+        return res.status(404).json({error:"Issue not found."}); 
+    }
+
+    const project = await Project.findById(issue.projectId).lean(); // Load parent project
+
+    if(!project) {
+        return res.status(404).json({error:"Project not found."}); // 404
+    } 
+
+    req.issue = issue;     // Attach issue to request
+    req.project = project; // Attach project to request (so RBAC can reuse)
+    next();                // Continue express pipeline
+  }
+  catch(err){ 
+    next(err); 
+  }
+};
